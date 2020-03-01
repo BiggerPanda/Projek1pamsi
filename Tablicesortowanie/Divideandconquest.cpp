5578e92361d069e36c9b5ecd3455741f78c9e695
BiggerPanda <robertmenel@gmail.com>
2020-03-01 19:50:47 +0100

    divide and sort in progress

--- Tablicesortowanie/Divideandconquest.cpp
+++ Tablicesortowanie/Divideandconquest.cpp
@@ -1,5 +1,12 @@
 #include "Divideandconquest.h"
 
-void divide()
+
+void divideandsort(int *tab,int left,int right)
 {
+	if (right <= left) { return; }
+
+	int mid = (left + right) / 2;
+
+	divideandsort(tab, mid+1, right);
+	divideandsort(tab, left, mid);
 }
--- Tablicesortowanie/Divideandconquest.h
+++ Tablicesortowanie/Divideandconquest.h
@@ -1,2 +1,2 @@
 #pragma once
-void divide();
+void divideandsort();
