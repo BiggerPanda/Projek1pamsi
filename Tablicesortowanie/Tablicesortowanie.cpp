9826f658a4c50eb3a640d1d81bdbe8dac652efad
BiggerPanda <robertmenel@gmail.com>
2020-03-04 18:22:43 +0100

    intiger array start

--- Tablicesortowanie/Tablicesortowanie.cpp
+++ Tablicesortowanie/Tablicesortowanie.cpp
@@ -11,37 +11,38 @@ int main()
     
     using namespace std;
 
-    
-    //int* tab; 
-    //int* tab_pom; 
-    //int n;
+    IntigerArr tab1(10000);
+    IntigerArr tab2(50000);
+    tab1.~IntigerArr();
+    tab2.~IntigerArr();
+    int* tab; 
+    int* tab_pom; 
+    int n;
 
-    //    //cout<<"Ile liczb będziesz chciał posortować? ";
-    //    cin >> n;
-    //    tab = new int[n]; //przydzielenie pamięci na elementy tablicy
-    //    tab_pom = new int[n];
+        //cout<<"Ile liczb będziesz chciał posortować? ";
+        cin >> n;
+        tab = new int[n]; //przydzielenie pamięci na elementy tablicy
+        tab_pom = new int[n];
         
-        //wczytanie liczb
-      /*  for (int i = 0; i < n; i++)
+       // wczytanie liczb
+        for (int i = 0; i < n; i++)
             cin >> tab[i];
 
         for (int i = 0; i < n; i++)
             cout << tab[i] << " ";
         cout << endl;
 
-        divideandsort(tab, 0, n - 1,tab_pom);*/
+        divideandsort(tab, 0, n - 1,tab_pom);
 
         //wypisanie posortowanych elementów
-    /*    for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             cout << tab[i] << " ";
 
         cin.ignore();
         cin.get();
         return 0;
- */   
-    cin.ignore();
-    cin.get();
-    return 0;
+ 
+   
 }
 
 
